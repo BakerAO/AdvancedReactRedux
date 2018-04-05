3ddc942a4e969e045cb9df6740762a2c18452aa4
BakerAO <dpshunter@gmail.com>
2018-04-05 13:16:52 -0500

    Initial Commit
    Signed-off-by: BakerAO <dpshunter@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+
+import reducers from './reducers';
+import HomePage from './components/index';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/" component={HomePage} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>,
+    document.querySelector('.container')
+);
